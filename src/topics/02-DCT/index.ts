7c0e15719f3ca244ae7937a523f2e9ea73f05d9f
abhayverma47 <vabhay2012@gmail.com>
2025-10-07 20:07:51 +0530

    DCTs video 1

--- src/topics/02-DCT/index.ts
+++ src/topics/02-DCT/index.ts
@@ -1,17 +1,15 @@
-//
-//
-//
+/****** Video 1 - What are DCTs *******/
 
 // Conditional types ask questions about types
 type IsString<T> = T extends string ? "YES" : "NO";
 
 // Works as expected with single types
-type Test1 = IsString<string>; //"YES" ✅
-type Test2 = IsString<number>; //"NO" ✅
+type Test1 = IsString<string>; // "YES" ✅
+type Test2 = IsString<number>; // "NO" ✅
 
 // But watch what happens with unions...
 type Test3 = IsString<string | number>;
 // Expected: "NO"(union is not exactly string)
-// Actual: "YES" | "NO" �
+// Actual: "YES" | "NO"
 
 // This is DISTRIBUTION in action!
